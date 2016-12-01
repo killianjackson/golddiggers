11c2b1ba055333378d02939dda35a1b0941ac545
Shan He <shanhex@gmail.com>
2016-11-30 20:54:06 -0800

    Update Naive_Bayes.py

--- Naive_Bayes.py
+++ Naive_Bayes.py
@@ -3,8 +3,8 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from sklearn import svm
-from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 
 def main():
 	X = pickle.load(open("Matrix/X.p", "rb"))
@@ -15,8 +15,8 @@ def main():
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 	nbc = MultinomialNB(alpha=0.19)
+	nbc = BernoulliNB(alpha=0.09)
 
-	# nbc = GaussianNB()
 	nbc.fit(X_train, y_train)
 	print "accuracy:"
 	print nbc.score(X_test, y_test)
