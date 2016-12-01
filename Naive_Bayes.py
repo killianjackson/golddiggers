1c78b9d0266f713e95d6b286b327d98d137d5842
Shan He <shanhex@gmail.com>
2016-11-30 19:47:07 -0800

    Add Naive Bayes Classifier

---
+++ Naive_Bayes.py
@@ -0,0 +1,25 @@
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+
+def main():
+	X = pickle.load(open("Matrix/X.p", "rb"))
+	y = pickle.load(open("Matrix/y.p", "rb"))
+
+	print X
+	print len(y)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+	nbc = MultinomialNB(alpha=0.05)
+
+	# nbc = GaussianNB()
+	nbc.fit(X_train.todense(), y_train)
+	print "accuracy:"
+	print nbc.score(X_test, y_test)
+
+if __name__ == '__main__':
+	main()
